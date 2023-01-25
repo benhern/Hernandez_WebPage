db30f4241081aace9d45a1e79a538529ecdd4b39
benhern <43895569+benhern@users.noreply.github.com>
2023-01-25 00:42:13 -0600

    Added in project columns

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    alert("ok");
+  }
+
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+knickText = document.getElementById('knick');
+knickText.style.color = "lightgrey";
+
+vr_pic = document.getElementById('VR-pic');
+vr_pic.style.color = "lightgrey";
+
